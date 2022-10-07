ea7e9dbc378fb4fd7152ac56f307c6a655255122
Brookzerker <brooks@brookzerker.com>
2022-10-06 19:50:42 -0600

    Working with state

--- yew_radio/src/components/multiple_normal.rs
+++ yew_radio/src/components/multiple_normal.rs
@@ -1,12 +1,41 @@
+use std::ops::Deref;
+
 use web_sys::HtmlInputElement;
 use yew::prelude::*;
 
+pub struct State {
+    pub animal: Animal,
+}
+
+#[derive(PartialEq)]
+pub enum Animal {
+    Dog,
+    Cat,
+}
+
 #[function_component(MultipleNormal)]
 pub fn multiple_normal() -> Html {
-    let onchange = Callback::from(|event: Event| {
-        let value = event.target_unchecked_into::<HtmlInputElement>().value();
-        gloo::console::log!(value);
+    let state = use_state(|| State {
+        animal: Animal::Dog,
     });
+
+    let onchange = {
+        let state = state.clone();
+        Callback::from(move |event: Event| {
+            let value = event.target_unchecked_into::<HtmlInputElement>().value();
+            let new_state = match value.as_ref() {
+                "cat" => State {
+                    animal: Animal::Cat,
+                },
+                "dog" => State {
+                    animal: Animal::Dog,
+                },
+                _ => unreachable!(),
+            };
+            state.set(new_state);
+            gloo::console::log!(value);
+        })
+    };
 
     html! {
         <form>
@@ -16,7 +45,7 @@ pub fn multiple_normal() -> Html {
                     <label for="cat">{"Cat"}</label>
                 </div>
                 <div>
-                    <input type="radio" id="cat" name="cat_or_dog" value="cat" onchange={onchange.clone()} />
+                    <input type="radio" id="cat" name="cat_or_dog" value="cat" onchange={onchange.clone()} checked={state.deref().animal == Animal::Cat} />
                 </div>
             </div>
             <div>
@@ -24,7 +53,7 @@ pub fn multiple_normal() -> Html {
                     <label for="dog">{"Dog"}</label>
                 </div>
                 <div>
-                    <input type="radio" id="dog" name="cat_or_dog" value="dog" onchange={onchange.clone()} />
+                    <input type="radio" id="dog" name="cat_or_dog" value="dog" onchange={onchange.clone()} checked={state.deref().animal == Animal::Dog} />
                 </div>
             </div>
         </form>
